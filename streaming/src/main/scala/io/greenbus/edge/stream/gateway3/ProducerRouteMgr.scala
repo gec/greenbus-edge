7d55a0c0720400208ed63db9dd6a79daf21c526d
devans <devans@greenenergycorp.com>
2017-05-28 16:42:03 -0400

    Producer code reorg

---
+++ streaming/src/main/scala/io/greenbus/edge/stream/gateway3/ProducerRouteMgr.scala
@@ -0,0 +1,180 @@
+/**
+ * Copyright 2011-2017 Green Energy Corp.
+ *
+ * Licensed to Green Energy Corp (www.greenenergycorp.com) under one or more
+ * contributor license agreements. See the NOTICE file distributed with this
+ * work for additional information regarding copyright ownership. Green Energy
+ * Corp licenses this file to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+package io.greenbus.edge.stream.gateway3
+
+import java.util.UUID
+
+import io.greenbus.edge.stream.{ PeerSessionId, TableRow }
+import io.greenbus.edge.stream.engine2.{ KeyStreamObserver, RouteTargetSubject, StreamObserver }
+import io.greenbus.edge.stream.gateway2.{ DynamicTable, ProducerStreamSubject }
+
+import scala.collection.mutable
+
+class ProducerRouteMgr extends RouteTargetSubject {
+
+  private var produced = false
+  private val updateMap = mutable.Map.empty[TableRow, ProducerUpdateStream]
+  private var dynamicTableMap = Map.empty[String, DynamicTable]
+
+  private val subjectMap = mutable.Map.empty[TableRow, ProducerStreamSubject]
+  private val subscriptionMap = mutable.Map.empty[StreamObserver, Map[TableRow, KeyStreamObserver]]
+
+  def bind(events: Seq[ProducerKeyEvent], dynamic: Map[String, DynamicTable]): Unit = {
+    unbind()
+    bindTables(dynamic)
+    events.foreach(handleEvent)
+    produced = true
+  }
+
+  def batch(events: Seq[ProducerKeyEvent]): Unit = {
+    events.foreach(handleEvent)
+  }
+
+  def unbind(): Unit = {
+    updateMap.clear()
+    dynamicTableMap = Map()
+    subjectMap.foreach { case (row, subj) => subj.unbind() }
+    produced = false
+  }
+
+  private def bindTables(dynamic: Map[String, DynamicTable]): Unit = {
+    dynamicTableMap = dynamic
+
+    val tableSubjects: Map[String, collection.Map[TableRow, ProducerStreamSubject]] =
+      subjectMap.filterKeys(row => dynamicTableMap.keySet.contains(row.table))
+        .groupBy(_._1.table)
+
+    tableSubjects.foreach {
+      case (table, rowMap) =>
+        dynamic.get(table).foreach { t =>
+          rowMap.keys.foreach(row => t.subscribed(row.rowKey))
+        }
+    }
+  }
+
+  private def handleEvent(ev: ProducerKeyEvent): Unit = {
+    ev match {
+      case AddRow(key, ctx) => {
+        //updateMap.get(key).foreach { stream => } // TODO: remove row?
+        val stream = new ProducerUpdateStream(PeerSessionId(UUID.randomUUID(), 0), ctx)
+        streamAdded(key, stream)
+      }
+      case RowUpdate(key, update) => {
+        updateMap.get(key).foreach(_.handle(update))
+      }
+      case DrowRow(key) =>
+        updateMap.get(key).foreach(s => streamRemoved(key, s))
+    }
+  }
+
+  private def streamAdded(key: TableRow, stream: ProducerUpdateStream): Unit = {
+    updateMap.update(key, stream)
+    subjectMap.get(key).foreach(subj => subj.bind(stream.cache))
+  }
+  private def streamRemoved(key: TableRow, stream: ProducerUpdateStream): Unit = {
+    updateMap -= key
+    subjectMap.get(key).foreach(subj => subj.unbind())
+  }
+
+  private def observerAdded(key: TableRow, observer: KeyStreamObserver): Unit = {
+    subjectMap.get(key) match {
+      case None =>
+        dynamicTableMap.get(key.table) match {
+          case None => staticSubjectAdded(key, observer)
+          case Some(table) => dynamicSubjectAdded(key, table, observer)
+        }
+      case Some(subject) =>
+        subject.targetAdded(observer)
+    }
+  }
+  private def observerRemoved(key: TableRow, observer: KeyStreamObserver): Unit = {
+    subjectMap.get(key).foreach { subject =>
+      subject.targetRemoved(observer)
+      if (!subject.targeted()) {
+        dynamicTableMap.get(key.table) match {
+          case None => staticSubjectRemoved(key, subject)
+          case Some(table) => dynamicSubjectRemoved(key, table, subject)
+        }
+      }
+    }
+  }
+
+  private def buildAndBindSubject(key: TableRow): ProducerStreamSubject = {
+    val subject = new ProducerStreamSubject
+    updateMap.get(key).foreach { producer =>
+      subject.bind(producer.cache)
+      producer.bind(subject)
+    }
+    subject
+  }
+
+  private def staticSubjectAdded(key: TableRow, observer: KeyStreamObserver): Unit = {
+    val subject = buildAndBindSubject(key)
+    subject.targetAdded(observer)
+  }
+  private def dynamicSubjectAdded(key: TableRow, table: DynamicTable, observer: KeyStreamObserver): Unit = {
+    val subject = buildAndBindSubject(key)
+    subject.targetAdded(observer)
+    table.subscribed(key.rowKey)
+  }
+  private def staticSubjectRemoved(key: TableRow, subject: ProducerStreamSubject): Unit = {
+    updateMap.get(key).foreach { stream => stream.unbind() }
+    subjectMap -= key
+  }
+  private def dynamicSubjectRemoved(key: TableRow, table: DynamicTable, subject: ProducerStreamSubject): Unit = {
+    updateMap.get(key).foreach { stream => stream.unbind() }
+    subjectMap -= key
+    table.unsubscribed(key.rowKey)
+  }
+
+  def targeted(): Boolean = {
+    subjectMap.keySet.nonEmpty || produced
+  }
+
+  def targetUpdate(target: StreamObserver, subscription: Map[TableRow, KeyStreamObserver]): Unit = {
+    val previous = subscriptionMap.getOrElse(target, Map())
+    val removes = previous.keySet -- subscription.keySet
+
+    removes.flatMap(row => previous.get(row).map(obs => (row, obs))).foreach {
+      case (key, obs) =>
+        observerRemoved(key, obs)
+    }
+
+    subscription.foreach {
+      case (key, obs) =>
+        observerAdded(key, obs)
+    }
+
+    val untargetedStreams = removes.filter(key => subjectMap.get(key).exists(!_.targeted()))
+    subjectMap --= untargetedStreams
+
+    subscriptionMap.update(target, subscription)
+  }
+
+  def targetRemoved(target: StreamObserver): Unit = {
+    val previous = subscriptionMap.getOrElse(target, Map())
+
+    previous.foreach {
+      case (key, obs) =>
+        observerRemoved(key, obs)
+    }
+
+    subscriptionMap -= target
+  }
+}
---
+++ streaming/src/main/scala/io/greenbus/edge/stream/gateway3/ProducerUpdateStream.scala
@@ -0,0 +1,58 @@
+/**
+ * Copyright 2011-2017 Green Energy Corp.
+ *
+ * Licensed to Green Energy Corp (www.greenenergycorp.com) under one or more
+ * contributor license agreements. See the NOTICE file distributed with this
+ * work for additional information regarding copyright ownership. Green Energy
+ * Corp licenses this file to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+package io.greenbus.edge.stream.gateway3
+
+import io.greenbus.edge.stream.{ PeerSessionId, SequenceCtx }
+import io.greenbus.edge.stream.engine2.StreamObserverSet
+import io.greenbus.edge.stream.filter.{ StreamCache, StreamCacheImpl }
+
+class ProducerUpdateStream(sessionId: PeerSessionId, ctx: SequenceCtx) {
+
+  private var sequencerOpt = Option.empty[UpdateSequencer]
+  private var observerOpt = Option.empty[StreamObserverSet]
+
+  protected val streamCache = new StreamCacheImpl
+
+  def cache: StreamCache = streamCache
+
+  def handle(update: ProducerDataUpdate): Unit = {
+
+    val sequencer = sequencerOpt.getOrElse {
+      val seq = UpdateSequencer.build(update, sessionId, ctx)
+      sequencerOpt = Some(seq)
+      seq
+    }
+
+    val sequenced = sequencer.handle(update)
+    sequenced.foreach(streamCache.handle)
+    observerOpt.foreach { set =>
+      set.observers.foreach { observer =>
+        sequenced.foreach(observer.handle)
+      }
+    }
+  }
+
+  def bind(set: StreamObserverSet): Unit = {
+    observerOpt = Some(set)
+  }
+
+  def unbind(): Unit = {
+    observerOpt = None
+  }
+}
