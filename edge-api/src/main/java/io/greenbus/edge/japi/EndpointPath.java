e0aebb2329e9dcf350108f1142feccba8f1b0dc9
devans <devans@greenenergycorp.com>
2017-06-13 22:35:50 -0400

    Fix for equals/hash

--- edge-api/src/main/java/io/greenbus/edge/japi/EndpointPath.java
+++ edge-api/src/main/java/io/greenbus/edge/japi/EndpointPath.java
@@ -34,4 +34,22 @@ public class EndpointPath {
     public Path getPath() {
         return path;
     }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        EndpointPath that = (EndpointPath) o;
+
+        if (endpointId != null ? !endpointId.equals(that.endpointId) : that.endpointId != null) return false;
+        return path != null ? path.equals(that.path) : that.path == null;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = endpointId != null ? endpointId.hashCode() : 0;
+        result = 31 * result + (path != null ? path.hashCode() : 0);
+        return result;
+    }
 }
